4e01185f77cbcd4973d07e69327d1fc38bb92ca4
mega7899 <enchantingsworf@yahoo.com>
2025-03-21 22:24:37 +0800

    Create style.css

---
+++ style.css
@@ -0,0 +1,117 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    background-color: hsl(186, 15%, 59%);
+}
+
+.form-container{
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 20px 15px;
+}
+
+.form-field{
+    font-size: 1.40rem;
+    margin: 10px 15px;
+}
+
+.input_style input{
+    border-radius: 5px;
+    width: 95%;
+    max-width: 1000;
+    padding: 10px;
+}
+
+#message-field{
+    border-radius: 5px;
+    width: 95%;
+    height: 170px;
+    resize: none;
+}
+
+.form-font{
+    font-size: 2rem;
+    margin: 25px 15px;
+}
+
+.radio-border{
+    border: 1px solid black;
+    border-radius: 10px;
+    width: 60%;
+    max-width: 1000;
+}
+
+.radio-border input{
+    margin: 0 10px;
+}
+
+.btn_style{
+    text-align: center;
+}
+
+.btn_style input{
+    background-color: hsl(169, 82%, 27%);
+    color: white;
+    border-radius: 10px;
+    font-size: 1.20rem;
+    width: 95%;
+    max-width: 1000;
+    padding: 10px;
+    margin: 10px 10px
+}
+
+.btn_style input:hover{
+    background-color: hsl(169, 75%, 24%);
+}
+
+@media screen and (min-width: 800px){
+    .form-container{
+        margin: 40px 50px;
+    }
+}
+
+@media screen and (min-width: 1000px){
+    .form{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-areas: 
+        "first last"
+        "email email"
+        "generalQuery supportQuery";
+        justify-content: center;
+        align-items: center;
+    }
+
+    .grid_first_name{
+        grid-area: first;
+    }
+
+    .grid_last_name{
+        grid-area: last;
+    }
+    .grid_email{
+        grid-area: email;
+    }
+    .grid_general_query{
+        grid-area: generalQuery;
+    }
+    .grid_support_query{
+        grid-area: supportQuery;
+    }
+
+    .container{
+        margin: 90px 80px;
+    }
+}
+
+@media screen and (min-width: 1500px){
+    .container{
+        width: 45%;
+        margin-left: 500px;
+    }
+}
